64ddf9be892a032cba06e6f35d44ce8f9a0dbb26
Gregor von Laszewski <laszewski@gmail.com>
2022-10-31 12:25:12 -0400

    pytest for delete

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -9,6 +9,7 @@ from pprint import pprint
 
 import pytest
 from yamldb.YamlDB import YamlDB
+import yaml
 
 from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh.common.util import HEADING
@@ -25,20 +26,25 @@ else:
 filename = path_expand(filename)
 
 
-
 data = {
     "floor": {
         "key": "value"
     }
 }
 
+def create_data_file(filename, data):
+    with open(filename, 'w') as file:
+        yaml.dump(dict(data), file)
+
 @pytest.mark.incremental
 class TestConfig:
 
     def test_delete(self):
         HEADING()
+        global data
+        print (data)
         StopWatch.start("init")
-        db = YamlDB(data=data, filename=filename)
+        db = YamlDB(data=dict(data), filename=filename)
         StopWatch.stop("init")
         db.save()
         content = readfile(filename)
@@ -62,7 +68,11 @@ class TestConfig:
 
     def test_read_from_file(self):
         HEADING()
+        global data
+        print("DDD", data)
         self.filename = "tests/test.yaml"
+
+        create_data_file(self.filename, data)
 
         banner(self.filename)
         content = readfile(self.filename)
@@ -92,10 +102,60 @@ class TestConfig:
         banner(self.filename)
         content = readfile(self.filename)
         print (content)
+    #
+
+    def test_delete_variants(self):
+        HEADING()
+        self.filename = "tests/test.yaml"
+
+        create_data_file(self.filename, data)
 
 
+        db = YamlDB(filename=self.filename)
+        db["floor.a"] = "value1"
+        db["floor.b"] = "value2"
+        db.save()
+
+        banner("db.data values")
+        print(db)
+
+        db.delete("floor.a")
+        db.save()
+
+        banner("db.data delete a")
+        print(db)
 
 
+        #
+        # THIS DOES NOT YET WORK
+        #
+        # db.delete["floor"]
+        # banner("db.data after delete")
+        # print(db)
+
+        #
+        # THIS IS NOT YET TESTED
+        #
+        #try:
+        #    db.delete["floor.key.nothing"]
+        #except Exception as e:
+        #    print (e)
+        #    assert "could not be found" in str(e)
+        #except TypeError:
+        #    print ("UUU")
+
+            # print (e)
+
+        # banner ("AAA")
+        # """
+        #
+        #
+        # del db["floor"]
+        # del db["floor.key"]
+        # del ["floor.key.nothing"]
+        # """
+
+class aaa:
 
     def test_StopWatch(self):
         StopWatch.benchmark(sysinfo=False)
